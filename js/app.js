9602081b684a6f7f3c6076eb37618adf18c42d47
Simon Schmid <zimonschmid@gmail.com>
2014-07-10 16:03:44 +0200

    added the collapse sections to all of trias

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-function showHide(shID) {
-   if (document.getElementById(shID)) {
-      if (document.getElementById(shID+'-show').style.display != 'none') {
-         document.getElementById(shID+'-show').style.display = 'none';
-         document.getElementById(shID).style.display = 'block';
-      }
-      else {
-         document.getElementById(shID+'-show').style.display = 'inline';
-         document.getElementById(shID).style.display = 'none';
-      }
-   }
-}
-
 jQuery(function() {
 	//catch all clicks on a tags
 	jQuery("a").click(function(){
