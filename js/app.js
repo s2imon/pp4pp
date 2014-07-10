f6b9bc5e057a7606a9267d994f81adc4bd3bc529
Simon Schmid <zimonschmid@gmail.com>
2014-07-10 15:45:14 +0200

    testing additional hiding mechanism 2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
-jQuery(function() {
+function showHide(shID) {
+   if (document.getElementById(shID)) {
+      if (document.getElementById(shID+'-show').style.display != 'none') {
+         document.getElementById(shID+'-show').style.display = 'none';
+         document.getElementById(shID).style.display = 'block';
+      }
+      else {
+         document.getElementById(shID+'-show').style.display = 'inline';
+         document.getElementById(shID).style.display = 'none';
+      }
+   }
+}j
+
+Query(function() {
 	//catch all clicks on a tags
 	jQuery("a").click(function(){
 		//check if it has a hash (i.e. if it's an anchor link)
